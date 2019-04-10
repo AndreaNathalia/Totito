9b3631ad3fcdfd50893bbadcd2544ad3c18f8cee
AndreaNathalia <andreanathaliar@gmail.com>
2019-04-10 00:46:25 -0600

    <fase 2 y fase 3>

--- Totito.py
+++ Totito.py
@@ -16,8 +16,8 @@ tablero= [["          |         |          "],\
           ["          |         |          "],\
  ]
 
-
-
+import random
+from random import choice
 
 # menu
 print("")
@@ -49,7 +49,7 @@ def ImprimirInstrucciones():
     print("/////////    INSTRUCCIONES:    /////////////////////////////////////////////////////////////////////////")
     print("")
     print("")
-    print("* Cuando sea tu turno presiona el número correspondiente a la posición en la que deseas poner tu dibujo *")  
+    print("* Cuando sea tu turno presiona la letra correspondiente a la posición en la que deseas poner tu dibujo *")  
     print("")
     print("                                               |     |     ")
     print("                                            Q  |  W  |  E  ")
@@ -133,6 +133,26 @@ def turno2():
 
 
 #----------------------------------------------------------------------------------------------
+# función para turno compu
+#TraducirPosicion = {"q":1, "w":2, "e":3, "a":4, "s":5, "d":6, "z":7, "x":8, "c": 9}
+#claves = TraducirPosicion.keys()
+posicionesTab = ["q", "w", "e", "a", "s", "d", "z", "x", "c"]
+def turnocompu():
+    #posicion = input("¿Qué posición quieres?: ", random.choice(claves))
+    posicion = (random.choice(posicionesTab))
+    print("¿Qué posición quieres?: ", posicion)
+    print("")
+
+    # cambiar el valor del indice por el dibujo
+    posicion = TraducirPosicion[posicion]
+    posicion = int(posicion) - 1
+    tablero2[posicion] = "    o    "
+    ImprimirTablero2()
+    return()
+#----------------------------------------------------------------------------------------------
+
+
+#----------------------------------------------------------------------------------------------
 # función para comprobar si las FILAS son iguales
 def CompararFilas():
     fila1 = tablero2[0] == tablero2[1] == tablero2[2] != "         "
@@ -183,12 +203,20 @@ def CompararDiagonales():
 #----------------------------------------------------------------------------------------------
 
 
-import random
+#-------------------------------------------------------------------------------------------------------------------------
+# función para escoger un nombre a la computadora
+#NombresCompu = ["Saku", "Manzi", "Hibiki", "Eiki", "Kiyuki", "Hono-hono", "Chizu", "Genki", "Itsuki", "Mitsu", "Haruka"]
+#def nombres():
+ #   computadora = (random.choice(NombresCompu))
+  #  print("Tu oponente es: ", computadora)
+
+   # return()
+#--------------------------------------------------------------------------------------------------------------------------
 
 
 
 
-# --------------------------------------- (fase 1) ---------------------------------------
+# ----------------------------------------------- (fase 1) -----------------------------------------------
 if opt == ("1"):
     print("")
     print("")
@@ -210,6 +238,9 @@ if opt == ("1"):
 
     # variable que guarda el nombre del jugador 2
     jugador2 = (input("JUGADOR 2: "))
+    print("")
+    print("                                      -  ", jugador1, "  vs  ", jugador2, "  -                      ")
+    print("")
     print("")
 
     # imprimir tablero
@@ -251,3 +282,137 @@ if opt == ("1"):
         print("--------------------------------->   ¡ G A N A S T E ", jugador2, "!")
         print("")
         print("")
+
+# --------------------------------------------------------------------------------------------------------
+
+
+# ----------------------------------------------- (fase 2) -----------------------------------------------
+if opt == ("2"):
+    print("")
+    print("")
+    print("")
+    print("-------------------------------------------------------------------------------------------------------")
+    print("")
+    print("                                           | F A S E  2 |                                              ")
+    print("")
+    print("")
+    print("")
+
+    # imprimir instrucciones:
+    ImprimirInstrucciones()
+    print("")
+
+    # variable que guarda el nombre del jugador 1
+    jugador1 = input("JUGADOR: ")
+    print("")
+
+    # escoger nombre random de la computadora
+    NombresCompu = ["Saku", "Manzi", "Hibiki", "Eiki", "Kiyuki", "Hono-hono", "Chizu", "Genki", "Itsuki", "Mitsu", "Haruka"]
+
+    computadora = (random.choice(NombresCompu))
+    print("OPONENTE: ", computadora)
+    print("")
+    print("                                      -  ", jugador1, "  vs  ", computadora, "  -                      ")
+    print("")
+    print("")
+
+
+    SeguirJuego = True
+
+    alternador = random.randint(0,1)
+
+# while para cambiar de turno
+    while SeguirJuego:
+        if alternador == 1:
+            print("Es turno de: ", jugador1, " ( x ) ")
+            print("")
+            turno1()
+            alternador = 0
+
+        
+        elif alternador == 0:
+            print("Es turno de: ", computadora, " ( o ) ")
+            print("")
+            turnocompu()
+            alternador = 1
+
+        SeguirJuego = CompararColumnas() and CompararFilas() and CompararDiagonales()
+
+
+
+    if alternador == 0:
+        print("--------------------------------->   ¡ G A N A S T E ", jugador1, "!")
+        print("")
+        print("")
+    
+    elif alternador == 1:
+        print("--------------------------------->   ¡ G A N A S T E ", computadora, "!")
+        print("")
+        print("")
+
+#---------------------------------------------------------------------------------------------------------------------------------
+
+
+
+# ----------------------------------------------- (fase 3) -----------------------------------------------
+if opt == ("3"):
+    print("")
+    print("")
+    print("")
+    print("-------------------------------------------------------------------------------------------------------")
+    print("")
+    print("                                           | F A S E  3 |                                              ")
+    print("")
+    print("")
+    print("")
+
+    # imprimir instrucciones:
+    ImprimirInstrucciones()
+    print("")
+
+    # variable que guarda el nombre del jugador 1
+    jugador1 = input("JUGADOR 1: ")
+    print("")
+
+    # variable que guarda el nombre del jugador 2
+    jugador2 = (input("JUGADOR 2: "))
+    print("")
+    print("                                      -  ", jugador1, "  vs  ", jugador2, "  -                      ")
+    print("")
+    print("")
+
+
+    SeguirJuego = True
+
+    alternador = random.randint(0,1)
+
+# while para cambiar de turno
+    while SeguirJuego:
+        if alternador == 1:
+            print("Es turno de: ", jugador1, " ( x ) ")
+            print("")
+            turno1()
+            alternador = 0
+
+        
+        elif alternador == 0:
+            print("Es turno de: ", jugador2, " ( o ) ")
+            print("")
+            turno2()
+            alternador = 1
+
+        SeguirJuego = CompararColumnas() and CompararFilas() and CompararDiagonales()
+
+
+
+    if alternador == 0:
+        print("--------------------------------->   ¡ G A N A S T E ", jugador2, "!")
+        print("")
+        print("")
+    
+    elif alternador == 1:
+        print("--------------------------------->   ¡ G A N A S T E ", jugador1, "!")
+        print("")
+        print("")
+
+# --------------------------------------------------------------------------------------------------------
